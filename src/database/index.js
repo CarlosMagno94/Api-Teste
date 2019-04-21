fc6821f755bbfbaa57c068af7d8fda78267fb56e
Carlos Magno <carlos@rastrocompleto.com.br>
2019-04-20 23:55:18 -0300

    commitando teste de api

--- src/database/index.js
+++ src/database/index.js
@@ -4,3 +4,9 @@ mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
+
+const mongoose = require('mongoose');
+const DB_URI = 'mongodb://localhost/mongobanco';
+mongoose.connect(DB_URI, {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+module.exports = mongoose;
