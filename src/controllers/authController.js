10f5df782d97edfe870f1d88c89cda7116facc60
Carlos Magno <carlos@rastrocompleto.com.br>
2019-04-22 02:31:09 -0300

    commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,54 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+
+const authConfig = require('../config/auth')
+
 const router = express.Router();
 
+function gerarToken(params = {}){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    });
+}
+
 router.post('/register', async (req, res) => {
+    const {email} = req.body;
+
     try{
+        if (await User.findOne({email})) {
+            return res.status(400).send({error: 'Usuário já existe!'});
+        }
         const user = await User.create(req.body);
-        return res.send({user});
+        user.password = undefined;
+
+        return res.send({
+            user,
+            token: gerarToken({ id: user.id }),
+        });
     }catch(err){
         return res.status(400).send({error: 'Falha ao registrar'});
+    }s
+});
+
+router.post('/authenticate', async (req, res) =>{
+    const {email, password} = req.body;
+    const user = await User.findOne({email}).select('+password');
+
+    if (!user) {
+        return res.status(400).send({ error: 'Usuário não encontrado!' });
     }
+    if (!await bcrypt.compare(password, user.password)) {
+        return res.status(400).send({error: 'Senha inválida!'});
+    }
+
+    user.password = undefined;
+
+    res.send({
+        user, 
+        token: gerarToken({ id: user.id }),
+    });
 });
 
 module.exports = app => app.use('/auth', router);
